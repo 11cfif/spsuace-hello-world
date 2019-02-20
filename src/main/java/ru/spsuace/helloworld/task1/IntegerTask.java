d36635dcbc561080d42b19711512fc5ec7897eb0
NicolayGeorgiev <YbRjKfQ5>
2019-02-20 19:55:45 +0300

    Added javadoc

--- src/main/java/ru/spsuace/helloworld/task1/IntegerTask.java
+++ src/main/java/ru/spsuace/helloworld/task1/IntegerTask.java
@@ -14,7 +14,11 @@ public class IntegerTask {
      * Пример: (5) -> 15
      */
     public static int sum(int n) {
-        return 0;
+        int sum = 0;
+        for (int i = 0; i <= n; i++) {
+            sum += i;
+        }
+        return sum;
     }
 
     /**
@@ -24,7 +28,22 @@ public class IntegerTask {
      * Пример: (10, 3, 2) -> 8
      */
     public static int snake(int height, int top, int bottom) {
-        return 0;
+        if (top >= height) {
+            return 1;
+        } else if (bottom >= top) {
+            return Integer.MAX_VALUE;
+        }
+        int day = 0;
+        int pos = 0;
+        while (height > pos) {
+            day++;
+            pos += top;
+            if (pos >= height) {
+                break;
+            }
+            pos -= bottom;
+        }
+        return day;
     }
 
     /**
@@ -32,7 +51,7 @@ public class IntegerTask {
      * Пример: (454355, 3) -> 3
      */
     public static int kDecimal(int n, int order) {
-        return 0;
+        return (int) Math.abs((n / Math.pow(10, order - 1)) % 10);
     }
 
 
@@ -41,6 +60,9 @@ public class IntegerTask {
      * Пример: (5) -> 120
      */
     public static long factorial(byte n) {
-        return 0;
+        if (n == 0) {
+            return 1;
+        }
+        return n * factorial((byte) (n - 1));
     }
 }
