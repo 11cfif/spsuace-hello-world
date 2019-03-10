47964b450262efe902947998e5d44ba5195ee50b
EvgenyaFyodorova <47686639+EvgenyaFyodorova@users.noreply.github.com>
2019-03-10 19:16:33 +0300

    Внесены изменения Фёдорова

--- src/main/java/ru/spsuace/helloworld/task1/DoubleTask.java
+++ src/main/java/ru/spsuace/helloworld/task1/DoubleTask.java
@@ -10,14 +10,23 @@ import ru.spsuace.helloworld.Main;
  */
 public class DoubleTask {
 
-    /**
-     * Вывести два корня квадратного уравнения через запятую: a * x ^ 2 + b * x + c = 0;
-     * Вывод менять не нужно, надо только посчитать x1 и x2, где x1 > x2
-     * Пример: (1, 5, 4) -> "-1.0, -4.0"
-     */
-    public static String equation(int a, int b, int c) {
-        double x1 = 0;
-        double x2 = 0;
+    public static void main(String[] args) {
+        String rez = equation(1, -3, 2);
+        System.out.println("Задание 1. Ответ: " + rez);
+        double R = length(1, -3, 2, 0);
+        System.out.println("Задание 2. Ответ: " + R);
+    }
+
+
+        /**
+         * Вывести два корня квадратного уравнения через запятую: a * x ^ 2 + b * x + c = 0;
+         * Вывод менять не нужно, надо только посчитать x1 и x2, где x1 > x2
+         * Пример: (1, 5, 4) -> "-1.0, -4.0"
+         */
+    public static String equation(double a, double b, double c) {
+        double D = Math.pow(b, 2) - (4 * a * c);
+        double x1 = (-b + Math.sqrt(D)) / (2 * a);
+        double x2 = (-b - Math.sqrt(D)) / (2 * a);
         return x1 + ", " + x2;
     }
 
@@ -25,7 +34,8 @@ public class DoubleTask {
      * Нужно посчитать расстояние, между двумя точками (x1, y1) и (x2, y2)
      * Пример: (0, 0, 3, 4) -> 5.0
      */
-    public static float length(double x1, double y1, double x2, double y2) {
-        return 0;
+    public static double length(double x1, double y1, double x2, double y2) {
+        double r = Math.sqrt(Math.pow((x2-x1), 2) + Math.pow((y2-y1), 2));
+        return r;
     }
 }
--- src/main/java/ru/spsuace/helloworld/task1/IntegerTask.java
+++ src/main/java/ru/spsuace/helloworld/task1/IntegerTask.java
@@ -9,12 +9,30 @@ package ru.spsuace.helloworld.task1;
  */
 public class IntegerTask {
 
+    public static void main(String[] args) {
+        int rez = sum(7);
+        System.out.println("Задание 1. Ответ: " + rez);
+
+        int t = snake(10,3,2);
+        System.out.println("Задание 2. Ответ: " + t);
+
+        int k = kDecimal(454355, 3);
+        System.out.println("Задание 3. Ответ: " + k);
+
+        long f = factorial((byte) 5);
+        System.out.println("Задание 4. Ответ: " + f);
+    }
+
     /**
      * Сумма чисел от 1 до n (1 + 2 + 3 + ... + n)
      * Пример: (5) -> 15
      */
     public static int sum(int n) {
-        return 0;
+        int s=0;
+        for (int i = 1 ; i <= n ; i++ ) {
+            s += i;
+        }
+        return s;
     }
 
     /**
@@ -24,7 +42,17 @@ public class IntegerTask {
      * Пример: (10, 3, 2) -> 8
      */
     public static int snake(int height, int top, int bottom) {
-        return 0;
+        int hp = top - bottom;//высота подъёма за сутки
+        int time;
+        if ( hp > 0 ) {
+            int pos = height - top; //высота, пройденная до последнего подъема
+            time = (pos / hp) + 1; // время подъема
+
+        } else {
+
+            time = Integer.MAX_VALUE;
+        }
+        return time;
     }
 
     /**
@@ -32,7 +60,14 @@ public class IntegerTask {
      * Пример: (454355, 3) -> 3
      */
     public static int kDecimal(int n, int order) {
-        return 0;
+        int[] c = new int[10];
+        int i=0;
+       for (int a = n; a > 0; a/=10){
+           c[i] = n % 10;
+           n /= 10;
+           i++;
+       }
+        return c[order-1];
     }
 
 
@@ -41,6 +76,10 @@ public class IntegerTask {
      * Пример: (5) -> 120
      */
     public static long factorial(byte n) {
-        return 0;
+        int f = 1;
+        for (int i = 1; i <= n; i++){
+            f = f*i;
+        }
+        return f;
     }
 }
