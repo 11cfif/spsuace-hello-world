bb7768edef119f9df9fe8841172f658d074d49d5
EvgenyaFyodorova <47686639+EvgenyaFyodorova@users.noreply.github.com>
2019-03-11 12:10:57 +0300

    Внесены правки

--- src/main/java/ru/spsuace/helloworld/task1/DoubleTask.java
+++ src/main/java/ru/spsuace/helloworld/task1/DoubleTask.java
@@ -10,19 +10,11 @@ import ru.spsuace.helloworld.Main;
  */
 public class DoubleTask {
 
-    public static void main(String[] args) {
-        String rez = equation(1, -3, 2);
-        System.out.println("Задание 1. Ответ: " + rez);
-        double R = length(1, -3, 2, 0);
-        System.out.println("Задание 2. Ответ: " + R);
-    }
-
-
-        /**
-         * Вывести два корня квадратного уравнения через запятую: a * x ^ 2 + b * x + c = 0;
-         * Вывод менять не нужно, надо только посчитать x1 и x2, где x1 > x2
-         * Пример: (1, 5, 4) -> "-1.0, -4.0"
-         */
+    /**
+     * Вывести два корня квадратного уравнения через запятую: a * x ^ 2 + b * x + c = 0;
+     * Вывод менять не нужно, надо только посчитать x1 и x2, где x1 > x2
+     * Пример: (1, 5, 4) -> "-1.0, -4.0"
+     */
     public static String equation(double a, double b, double c) {
         double D = Math.pow(b, 2) - (4 * a * c);
         double x1 = (-b + Math.sqrt(D)) / (2 * a);
@@ -35,7 +27,7 @@ public class DoubleTask {
      * Пример: (0, 0, 3, 4) -> 5.0
      */
     public static double length(double x1, double y1, double x2, double y2) {
-        double r = Math.sqrt(Math.pow((x2-x1), 2) + Math.pow((y2-y1), 2));
+        double r = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
         return r;
     }
 }
--- src/main/java/ru/spsuace/helloworld/task1/IntegerTask.java
+++ src/main/java/ru/spsuace/helloworld/task1/IntegerTask.java
@@ -1,6 +1,8 @@
 package ru.spsuace.helloworld.task1;
 
 
+import static java.lang.Math.ceil;
+
 /**
  * Возможно вам понадобится класс Math с его методами. Например, чтобы вычислить квадратный корень, достаточно написать
  * Math.sqrt(1.44)
@@ -9,27 +11,13 @@ package ru.spsuace.helloworld.task1;
  */
 public class IntegerTask {
 
-    public static void main(String[] args) {
-        int rez = sum(7);
-        System.out.println("Задание 1. Ответ: " + rez);
-
-        int t = snake(10,3,2);
-        System.out.println("Задание 2. Ответ: " + t);
-
-        int k = kDecimal(454355, 3);
-        System.out.println("Задание 3. Ответ: " + k);
-
-        long f = factorial((byte) 5);
-        System.out.println("Задание 4. Ответ: " + f);
-    }
-
     /**
      * Сумма чисел от 1 до n (1 + 2 + 3 + ... + n)
      * Пример: (5) -> 15
      */
     public static int sum(int n) {
-        int s=0;
-        for (int i = 1 ; i <= n ; i++ ) {
+        int s = 0;
+        for (int i = 1; i <= n; i++) {
             s += i;
         }
         return s;
@@ -42,15 +30,18 @@ public class IntegerTask {
      * Пример: (10, 3, 2) -> 8
      */
     public static int snake(int height, int top, int bottom) {
-        int hp = top - bottom;//высота подъёма за сутки
-        int time;
-        if ( hp > 0 ) {
-            int pos = height - top; //высота, пройденная до последнего подъема
-            time = (pos / hp) + 1; // время подъема
+        double hp = top - bottom;//высота подъёма за сутки
+        int time = 0;
+        if (hp > 0) {
+            double pos = height - top; //высота, пройденная до последнего подъема
+            time = (int) (Math.ceil(pos / hp) + 1); // время подъема
 
-        } else {
-
+        }
+        if (hp <= 0) {
             time = Integer.MAX_VALUE;
+        }
+        if ((height - top) == 0) {
+            time = 1;
         }
         return time;
     }
@@ -61,13 +52,13 @@ public class IntegerTask {
      */
     public static int kDecimal(int n, int order) {
         int[] c = new int[10];
-        int i=0;
-       for (int a = n; a > 0; a/=10){
-           c[i] = n % 10;
-           n /= 10;
-           i++;
-       }
-        return c[order-1];
+        int i = 0;
+        for (int a = Math.abs(n); a > 0; a /= 10) {
+            c[i] = Math.abs(n % 10);
+            n /= 10;
+            i++;
+        }
+        return c[order - 1];
     }
 
 
@@ -76,9 +67,9 @@ public class IntegerTask {
      * Пример: (5) -> 120
      */
     public static long factorial(byte n) {
-        int f = 1;
-        for (int i = 1; i <= n; i++){
-            f = f*i;
+        long f = 1;
+        for (int i = 1; i <= n; i++) {
+            f = f * i;
         }
         return f;
     }
