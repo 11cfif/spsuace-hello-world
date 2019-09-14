97b00457e424155489e93a25e14426999107aa06
olkhovsky <olkhovskii_av@ascon.ru>
2019-09-14 21:28:21 +0300

    Сделал IntegerTask. Все тесты зеленые

--- src/main/java/ru/spsuace/helloworld/Main.java
+++ src/main/java/ru/spsuace/helloworld/Main.java
@@ -1,12 +1,15 @@
 package ru.spsuace.helloworld;
 
+import ru.spsuace.helloworld.task1.IntegerTask;
+import ru.spsuace.helloworld.task1.MainTask;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println(hello());
-    }
+   }
 
     static String hello() {
-        return "Looks like I'm running...";
+        return "Hello world! I am a first program";
     }
 }
--- src/main/java/ru/spsuace/helloworld/task1/IntegerTask.java
+++ src/main/java/ru/spsuace/helloworld/task1/IntegerTask.java
@@ -1,6 +1,8 @@
 package ru.spsuace.helloworld.task1;
 
 
+import com.sun.javafx.runtime.async.AbstractRemoteResource;
+
 /**
  * Возможно вам понадобится класс Math с его методами. Например, чтобы вычислить квадратный корень, достаточно написать
  * Math.sqrt(1.44)
@@ -13,7 +15,10 @@ public class IntegerTask {
      * Сумма чисел от 1 до n (1 + 2 + 3 + ... + n)
      * Пример: (5) -> 15
      */
-    public static int sum(int n) {
+    public static int sum(int n)
+    {
+        if(n>0)
+            return sum(n - 1) + n;
         return 0;
     }
 
@@ -23,16 +28,34 @@ public class IntegerTask {
      * Верните число Integer.MAX_VALUE;
      * Пример: (10, 3, 2) -> 8
      */
-    public static int snake(int height, int top, int bottom) {
-        return 0;
+    public static int snake(int height, int top, int bottom)
+    {
+        if((top - bottom <= 0) && (height > top))
+            return  Integer.MAX_VALUE;
+
+        int days =0, passed = 0;
+        while(true)
+        {
+            days++;
+            passed+=top;
+            if(passed>=height)
+                break;
+            passed-=bottom;
+        }
+
+        return days;
     }
+
 
     /**
      * Дано число n и номер разряда order. Выведите цифру стояющую на нужном разряде
      * Пример: (454355, 3) -> 3
      */
-    public static int kDecimal(int n, int order) {
-        return 0;
+    public static int kDecimal(int n, int order)
+    {
+        for(int i = 1; i < order; i++)
+            n = n / 10;
+        return Math.abs(n % 10);
     }
 
 
@@ -40,7 +63,10 @@ public class IntegerTask {
      * Выведите факториал от числа n
      * Пример: (5) -> 120
      */
-    public static long factorial(byte n) {
-        return 0;
+    public static long factorial(byte n)
+    {
+        if (n <= 1)
+            return 1;
+        return  factorial((byte)(n -1)) *n;
     }
 }
