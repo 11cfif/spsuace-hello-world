3f2e2060d672c10b684c11474e34785651eb3513
Iliya <boitsov-iliya@yandex.ru>
2019-09-15 21:02:38 +0300

    ПРОБЕЛЫ + имена переменных

--- src/main/java/ru/spsuace/helloworld/task1/IntegerTask.java
+++ src/main/java/ru/spsuace/helloworld/task1/IntegerTask.java
@@ -14,11 +14,11 @@ public class IntegerTask {
      * Пример: (5) -> 15
      */
     public static int sum(int n) {
-        int S = 0;
-        for(int i=1;i<=n;i++){
-            S += i;
+        int s = 0;
+        for (int i = 1; i <= n; i++) {
+            s += i;
         }
-        return S;
+        return s;
     }
 
     /**
@@ -29,14 +29,14 @@ public class IntegerTask {
      */
     public static int snake(int height, int top, int bottom) {
         int days = Integer.MAX_VALUE;
-        if (height<=top){
-            days=1;
+        if (height <= top) {
+            days = 1;
         }
-        if (top>bottom){
-            days=1;
-            int current_height=top;
-            while(current_height<height){
-               current_height+=top-bottom;
+        if (top > bottom) {
+            days = 1;
+            int current_height = top;
+            while (current_height < height) {
+               current_height += top-bottom;
                days++;
             }
         }
@@ -48,9 +48,9 @@ public class IntegerTask {
      * Пример: (454355, 3) -> 3
      */
     public static int kDecimal(int n, int order) {
-        int t=n;
-        for (int i=1;i<order;i++){
-           t = t/10;
+        int t = n;
+        for (int i = 1; i < order; i++) {
+           t = t / 10;
         }
         return Math.abs(t % 10);
     }
@@ -61,10 +61,10 @@ public class IntegerTask {
      * Пример: (5) -> 120
      */
     public static long factorial(byte n) {
-       if (n==0) {
+       if (n == 0) {
            return 1;
        } else {
-           return n * factorial((byte) (n-1));
+           return n * factorial( (byte) (n - 1));
        }
     }
 }
