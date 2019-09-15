9d0a772a998504f3bd9026df6b6a7a999244b9fc
popovaleona <popovaleona@gmail.com>
2019-09-15 23:01:43 +0300

    Added javadoc

--- src/main/java/ru/spsuace/helloworld/Main.java
+++ src/main/java/ru/spsuace/helloworld/Main.java
@@ -3,10 +3,6 @@ package ru.spsuace.helloworld;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(hello());
-    }
-
-    static String hello() {
-        return "Hello world! I am а first program.";
+        System.out.println("hello_world");
     }
 }
