88d8000358148572eee0d5a698959c9cb6499218
Nataly9898 <natalya.afimchenko@mail.ru>
2019-09-23 16:07:24 +0300

    Afimchenko

--- src/main/java/ru/spsuace/helloworld/task1/DoubleTask.java
+++ src/main/java/ru/spsuace/helloworld/task1/DoubleTask.java
@@ -15,12 +15,12 @@ public class DoubleTask {
      * Вывод менять не нужно, надо только посчитать x1 и x2, где x1 > x2
      * Пример: (1, 5, 4) -> "-1.0, -4.0"
      */
-    public static String equation(int a, int b, int c){
-        double d=Math.pow(b, 2)-(4*a*c);
-        double x1=(-b+Math.sqrt(d))/(2.0*a);
-        double x2=(-b-Math.sqrt(d))/(2.0*a);
+    public static String equation(int a, int b, int c) {
+        double d = Math.pow(b, 2) - (4 * a * c);
+        double x1 = (-b + Math.sqrt(d)) / (2.0 * a);
+        double x2 = (-b - Math.sqrt(d)) / (2.0 * a);
 
-        if (x1>x2){
+        if (x1 > x2) {
             return x1 + ", " + x2;
         } else {
             return x2 + ", " + x1;
@@ -32,9 +32,9 @@ public class DoubleTask {
      * Пример: (0, 0, 3, 4) -> 5.0
      */
     public static float length(double x1, double y1, double x2, double y2) {
-        double differenceX=Math.pow((x2-x1),2);
-        double differenceY=Math.pow((y2-y1),2);
-        float distance=(float)Math.sqrt(differenceX+differenceY);
+        double differenceX = Math.pow((x2 - x1), 2);
+        double differenceY = Math.pow((y2 - y1), 2);
+        float distance = (float) Math.sqrt(differenceX + differenceY);
         return distance;
     }
 }
