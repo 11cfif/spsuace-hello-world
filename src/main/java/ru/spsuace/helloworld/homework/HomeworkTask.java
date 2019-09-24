bc50d7fa2b3469dfd1071b2da3e6924e70b4eddb
olkhovsky <olkhovskii_av@ascon.ru>
2019-09-24 14:10:54 +0300

    Недоделано

--- src/main/java/ru/spsuace/helloworld/homework/HomeworkTask.java
+++ src/main/java/ru/spsuace/helloworld/homework/HomeworkTask.java
@@ -1,5 +1,5 @@
 package ru.spsuace.helloworld.homework;
-
+import java.util.*;
 import java.util.function.ToDoubleFunction;
 
 public class HomeworkTask {
@@ -9,8 +9,13 @@ public class HomeworkTask {
      * Для того, что бы получить значение по Y для точки X, надо просто написать function.applyAsDouble(t)
      * Считаем, что функция определена на всем пространстве от a до b
      */
-    public static double calcIntegral(double a, double b, ToDoubleFunction<Double> function, double delta)
-    {
+    public static double calcIntegral(double a, double b, ToDoubleFunction<Double> function, double delta) {
+        if (b < a) {
+            double cash = a;
+            a = b;
+            b = cash;
+        }
+
         double sum = 0;
         for (double i = a; i < b; i += delta) {
             sum += function.applyAsDouble(i) * delta;
@@ -23,22 +28,21 @@ public class HomeworkTask {
      * выводим номер первой максимальной цифры (если их несколько)
      */
     public static byte maxNumber(long a) {
-        byte maxNumber = 0;
+        long copyA = a;
+        int maxNumber = 1;
         int number;
-        byte invertResult = 0;
-        byte i;
-        for(i = 1; true; i++)
-        {
-            number = (int) a % 10;
-            if (number >= maxNumber)
-            {
-                maxNumber = (byte) number;
+        int invertResult = 1;
+        int i;
+        for(i = 1; ; i++) {
+            number = (int) copyA % 10;
+            if (number >= maxNumber) {
+                maxNumber = number;
                 invertResult = i;
             }
-            if( a < 10) {
+            if( copyA < 10) {
                 break;
             } else {
-                a /= 10;
+                copyA /= 10;
             }
         }
         return (byte)(i + 1 - invertResult);
@@ -50,8 +54,7 @@ public class HomeworkTask {
      * которая находится на той же прямой что и первые две.
      */
     public static double lineFunction(int x1, int y1, int x2, int y2, int x3) {
-        ToDoubleFunction<Integer> line = x -> (x - x1) * (y2 - y1) / (x2 - x1) + y1;
-        return line.applyAsDouble(x3);
+        return (x3 - x1) * (y2 - y1) / (x2 - x1) + y1;
     }
 
     /**
@@ -60,7 +63,58 @@ public class HomeworkTask {
      * Это дополнительное задание, необязательное для выполнения
      */
     public static double square(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4) {
-        return 0;
+        ArrayList<Coord> coords = new ArrayList<Coord>();
+        coords.add(new Coord(x1, y1));
+        coords.add(new Coord(x2, y2));
+        coords.add(new Coord(x3, y3));
+        coords.add(new Coord(x4, y4));
+
+        Collections.sort(coords, new Coord(0,0));
+        int sign = 1;
+        if ((coords.get(0).X > coords.get(1).X) && (coords.get(2).X < coords.get(3).X)){
+            Coord cash = coords.get(0);
+            coords.set(0, coords.get(1));
+            coords.set(1, cash);
+            sign *= -1;
+        }
+
+
+        ToDoubleFunction<Double> top = x -> (x - coords.get(0).X) * (coords.get(1).Y - coords.get(0).Y) / (coords.get(1).X - coords.get(0).X) + coords.get(0).Y;
+        ToDoubleFunction<Double> bottom = x -> (x - coords.get(2).X) * (coords.get(3).Y - coords.get(2).Y) / (coords.get(3).X - coords.get(2).X) + coords.get(2).Y;
+
+        ToDoubleFunction<Double> left = x -> (x - coords.get(0).X) * (coords.get(2).Y - coords.get(0).Y) / (coords.get(2).X - coords.get(0).X) + coords.get(0).Y;
+        ToDoubleFunction<Double> right = x -> (x - coords.get(1).X) * (coords.get(3).Y - coords.get(1).Y) / (coords.get(3).X - coords.get(1).X) + coords.get(1).Y;
+
+        double sBottom = calcIntegral(coords.get(0).X, coords.get(1).X, top, 1e-3);
+        double sTop = calcIntegral(coords.get(2).X, coords.get(3).X, bottom, 1e-3);
+
+        double sLeft = calcIntegral(coords.get(0).X, coords.get(2).X, left, 1e-3);
+        double sRight = calcIntegral(coords.get(1).X, coords.get(3).X, right, 1e-3);
+
+        return (sTop - sBottom) + Math.abs(sRight - sLeft);
     }
 
 }
+
+class Coord implements Comparator<Coord> {
+    public int X;
+    public int Y;
+
+    public Coord(int x, int y) {
+        X = x;
+        Y = y;
+    }
+
+    @Override
+    public int compare(Coord c1, Coord c2) {
+        int deltaY = c1.Y - c2.Y;
+        if (deltaY == 0){
+            int deltaX = c1.X - c2.X;
+            return  deltaX;
+        } else {
+            return  deltaY;
+        }
+    }
+}
+
+
