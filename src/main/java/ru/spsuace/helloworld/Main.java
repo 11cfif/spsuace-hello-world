177cd0d9817cd703337995d004a6bd6d61952fa3
19000157 <xxxamyakov@mail.ru>
2020-02-15 09:40:17 +0300

    Test

--- src/main/java/ru/spsuace/helloworld/Main.java
+++ src/main/java/ru/spsuace/helloworld/Main.java
@@ -4,9 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println(hello());
+        System.out.println("Test");
     }
 
     static String hello() {
         return "Hello world! I am а first program.";
     }
+
 }
