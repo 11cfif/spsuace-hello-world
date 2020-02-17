4310a5cfdcf34a0bf2dadc47670b08b9ceeba519
vadklochkov <vad.klochkov@gmail.com>
2020-02-17 13:48:09 +0300

    Сделал первую задачу и начал вторую.

--- src/main/java/ru/spsuace/helloworld/task1/IntegerTask.java
+++ src/main/java/ru/spsuace/helloworld/task1/IntegerTask.java
@@ -14,7 +14,11 @@ public class IntegerTask {
      * Пример: (5) -> 15
      */
     public static int sum(int n) {
-        return 0;
+        int sum = 0;
+        for (int i = 0; i < n; i += 1){
+            sum = sum + i + 1;
+        }
+        return sum;
     }
 
     /**
@@ -24,7 +28,11 @@ public class IntegerTask {
      * Пример: (10, 3, 2) -> 8
      */
     public static int snake(int height, int top, int bottom) {
-        return 0;
+        int propolzla = 0;
+        int days = 0;
+        
+
+        return Integer.Max_value;
     }
 
     /**
