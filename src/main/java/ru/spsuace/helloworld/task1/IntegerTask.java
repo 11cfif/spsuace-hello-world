fad1db0fec7d30dc0b66f6981b437f8026c9a5be
vadklochkov <vad.klochkov@gmail.com>
2020-02-20 21:15:11 +0300

    Доделал IntegerTask.java

--- src/main/java/ru/spsuace/helloworld/task1/IntegerTask.java
+++ src/main/java/ru/spsuace/helloworld/task1/IntegerTask.java
@@ -68,6 +68,10 @@ public class IntegerTask {
      * Пример: (5) -> 120
      */
     public static long factorial(byte n) {
-        return 0;
+        long factorial = 1;
+        for(int i = 1; i <= n; i += 1){
+            factorial = factorial * i;
+        }
+        return factorial;
     }
 }
