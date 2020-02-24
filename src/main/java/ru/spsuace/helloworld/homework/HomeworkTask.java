65c00e4f4c1dd05578e91081f388907c8b138dea
raskiri <raskihi13@gmail.com>
2020-02-25 01:24:04 +0300

    new homework 2/4

--- src/main/java/ru/spsuace/helloworld/homework/HomeworkTask.java
+++ src/main/java/ru/spsuace/helloworld/homework/HomeworkTask.java
@@ -18,7 +18,18 @@ public class HomeworkTask {
      * выводим номер первой максимальной цифры (если их несколько)
      */
     public static byte maxNumber(long a) {
-        return 0;
+        int max=0;
+        int maxNum=0;
+        int numeral=0;
+        for(int i =  String.valueOf(Math.abs(a)).length();i>0;--i) {
+            numeral= (int) Math.abs(a % 10);
+            if (numeral>=max){
+                max=numeral;
+                maxNum=i;
+            }
+            a=a/10;
+        }
+        return (byte) maxNum;
     }
 
 
@@ -27,7 +38,11 @@ public class HomeworkTask {
      * которая находится на той же прямой что и первые две.
      */
     public static double lineFunction(int x1, int y1, int x2, int y2, int x3) {
-        return 0;
+        int x12=x2-x1;
+        int y12=y2-y1;
+        double x13=x3-x1;
+       double k=x13/x12;
+        return k*y12+y1;
     }
 
     /**
