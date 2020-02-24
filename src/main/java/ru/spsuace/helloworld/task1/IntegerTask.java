3082e28d59beef4aad07de980e2643f5b1359f8d
19000154 <pavlikovan2000@gmail.com>
2020-02-24 21:18:08 +0300

    Павликоа Анастасия 1-пми-1

--- src/main/java/ru/spsuace/helloworld/task1/IntegerTask.java
+++ src/main/java/ru/spsuace/helloworld/task1/IntegerTask.java
@@ -14,7 +14,11 @@ public class IntegerTask {
      * Пример: (5) -> 15
      */
     public static int sum(int n) {
-        return 0;
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum = sum + i + 1;
+        }
+        return sum;
     }
 
     /**
@@ -24,23 +28,51 @@ public class IntegerTask {
      * Пример: (10, 3, 2) -> 8
      */
     public static int snake(int height, int top, int bottom) {
-        return 0;
+        int snake = 1;
+        if (top > bottom) {
+            for (int i = top; i < height; i = i + top) {
+                snake++;
+                if (i < height) {
+                    i = i - bottom;
+                } else {
+                    return snake;
+                }
+            }
+        }
+        if (top == height) {
+            return (1);
+        }
+        if (top <= bottom && top < height) {
+            return (Integer.MAX_VALUE);
+        }
+        return snake;
     }
+
 
     /**
      * Дано число n и номер разряда order. Выведите цифру стояющую на нужном разряде
      * Пример: (454355, 3) -> 3
      */
     public static int kDecimal(int n, int order) {
-        return 0;
-    }
+        if (n < 0) {
+            n = n * (-1);
+        }
+        int kDecimal = (int) ((n % Math.pow(10, order) - n % Math.pow(10, order - 1)) / Math.pow(10, order - 1));
 
+        {
+            return kDecimal;
+        }
+    }
 
     /**
      * Выведите факториал от числа n
      * Пример: (5) -> 120
      */
     public static long factorial(byte n) {
-        return 0;
+        long factorial = 1;
+        for (int i = 1; i <= n; i++) {
+            factorial = factorial*i;
+        }
+        return factorial;
     }
 }
