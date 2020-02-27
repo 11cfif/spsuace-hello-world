99b78791dc98cf27c9ba95138897b6800c13e22c
UyraIV <UyraIV@mail.ru>
2020-02-27 17:34:14 +0300

    v.0006

--- src/main/java/ru/spsuace/helloworld/task1/MainTask.java
+++ src/main/java/ru/spsuace/helloworld/task1/MainTask.java
@@ -5,9 +5,15 @@ public class MainTask {
     /**
      * Сделать так, чтобы класс компилировался, при раскомментировании метода ниже
      * Так же, надо сделать так, что бы при запуске этого класса печаталась строка "It is work."
-     * @param args
+     *
+     * @param argsa
      */
-//    private static main(String args) {
-//        System.out.println("It is work.");
-//    }
+    private static void main(String[] args) {
+        System.out.println(maintask());
+    }
+
+    static String maintask() {
+        return "It is work.";
+    }
 }
+
---
+++ src/test/java/ru/spsuace/helloworld/task1/MainTaskTest.java
@@ -0,0 +1,12 @@
+package ru.spsuace.helloworld.task1;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainTaskTest {
+
+    @Test
+    public void maintask() {
+        Assert.assertEquals("It is work.", );
+    }
+}
