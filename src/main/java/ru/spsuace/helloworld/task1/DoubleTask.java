8a4e83c5e0d5a4117f36a99795a741470fbf5ec0
mashakryuchkova <masha.kryuchkova17@mail.ru>
2020-02-28 17:50:28 +0300

    double task, integer task

--- src/main/java/ru/spsuace/helloworld/task1/DoubleTask.java
+++ src/main/java/ru/spsuace/helloworld/task1/DoubleTask.java
@@ -15,10 +15,28 @@ public class DoubleTask {
      * Вывод менять не нужно, надо только посчитать x1 и x2, где x1 > x2
      * Пример: (1, 5, 4) -> "-1.0, -4.0"
      */
-    public static String equation(int a, int b, int c) {
-        double x1 = 0;
-        double x2 = 0;
-        return x1 + ", " + x2;
+    public static String equation(int a, int b, int c, double D) {
+
+System.out.println("Решение квадратного уравнения ax^2+bx+c=0. Введите a,b,c: ");
+        a = in.nextInteger();
+        b = in.nextInteger();
+        c = in.nextInteger();
+D = b*b-4*a*c;
+if (D>0) {
+    double x1 = 0;
+    double x2 = 0;
+    x1 = (-b - Math.sqrt(D)) / (2 * a);
+    x2 = (-b + Math.sqrt(D)) / (2 * a);
+    System.out.println("Корни уравнения: x1 = " + x1 + ", x2 = " + x2);
+}
+else if (D == 0) {
+    double x;
+    x = -b/(2*a);
+    System.out.println("Единственный корень: x = " + x);
+}
+else {
+    System.out.println("Действительных корней нет");
+}
     }
 
     /**
@@ -26,6 +44,7 @@ public class DoubleTask {
      * Пример: (0, 0, 3, 4) -> 5.0
      */
     public static float length(double x1, double y1, double x2, double y2) {
+        double d = Math.sqrt((x2 - x1)*(x2-x1)+(y2-y1)*(y2-y1));
         return 0;
     }
 }
