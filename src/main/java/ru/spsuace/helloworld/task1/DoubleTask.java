3d63226ede7b44dc609fefa7c9f7afeddcbe6986
UyraIV <UyraIV@mail.ru>
2020-03-06 22:02:04 +0300

    v.0031

--- src/main/java/ru/spsuace/helloworld/task1/DoubleTask.java
+++ src/main/java/ru/spsuace/helloworld/task1/DoubleTask.java
@@ -13,13 +13,13 @@ public class DoubleTask {
      * Пример: (1, 5, 4) -> "-1.0, -4.0"
      */
     public static String equation(int a, int b, int c) {
-        double d = Math.sqrt(Math.pow(b, 2) - 4 * a * c);
-        double x1 = (-b + d) / (2 * a);
-        double x2 = (-b - d) / (2 * a);
+        double dis = Math.sqrt(Math.pow(b, 2) - 4 * a * c);
+        double x1 = (-b + dis) / (2 * a);
+        double x2 = (-b - dis) / (2 * a);
         if (x2 > x1) {
-            double v = x1;
+            double var = x1;
             x1 = x2;
-            x2 = v;
+            x2 = var    ;
         }
         return x1 + ", " + x2;
     }
