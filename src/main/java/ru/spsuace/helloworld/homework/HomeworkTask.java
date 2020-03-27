66b9272a325c0112fa0e07e9cd369174fd91d5db
Ko6ara <anastasiya.golubeva90@mail.ru>
2020-03-27 18:53:47 +0300

    Голубева Анастасия 2-ПМИ-1

--- src/main/java/ru/spsuace/helloworld/homework/HomeworkTask.java
+++ src/main/java/ru/spsuace/helloworld/homework/HomeworkTask.java
@@ -10,7 +10,24 @@ public class HomeworkTask {
      * Считаем, что функция определена на всем пространстве от a до b
      */
     public static double calcIntegral(double a, double b, ToDoubleFunction<Double> function, double delta) {
-        return 0;
+        int n = (int) ((b - a) / delta);
+        double sumEvenPoints = 0, sumOddPoints = 0, firstPoint = function.applyAsDouble(a + delta),
+                lastPoint = function.applyAsDouble(b - delta);
+
+        for (int i = 1; i < n; i++) {
+            double point = function.applyAsDouble(a + delta * i);
+
+            if (i % 2 == 0) {
+                sumEvenPoints += point;
+            } else {
+                sumOddPoints += point;
+            }
+        }
+
+        double result = sumEvenPoints * 2 + sumOddPoints * 4 + firstPoint + lastPoint;
+        result *= delta / 3;
+
+        return result;
     }
 
     /**
@@ -18,16 +35,46 @@ public class HomeworkTask {
      * выводим номер первой максимальной цифры (если их несколько)
      */
     public static byte maxNumber(long a) {
-        return 0;
-    }
+        if (a < 10) {
+            return 1;
+        }
 
+        long currentValue = a;
+        byte maxNumber = 0;
+        byte maxNumberIndex = 0;
+        byte index = 0;
+
+        while (currentValue >= 10) {
+            byte currentNumber = (byte) (currentValue % 10);
+            currentValue = currentValue / 10;
+
+            if (currentNumber >= maxNumber) {
+                maxNumber = currentNumber;
+                maxNumberIndex = index;
+            }
+
+            index++;
+        }
+
+        if (currentValue >= maxNumber)  {
+            maxNumberIndex = index;
+        }
+
+        return (byte) (index - maxNumberIndex + 1);
+    }
 
     /**
      * Даны две точки в пространстве (x1, y1) и (x2, y2). Вам нужно найти Y координату третьей точки (x3, y3),
      * которая находится на той же прямой что и первые две.
      */
     public static double lineFunction(int x1, int y1, int x2, int y2, int x3) {
-        return 0;
+        double a = y1 - y2;
+        double b = x2 - x1;
+        double c = x1 * y2 - x2 * y1;
+
+        double y3 = (-a * x3 - c) / b;
+
+        return y3;
     }
 
     /**
@@ -36,7 +83,10 @@ public class HomeworkTask {
      * Это дополнительное задание, необязательное для выполнения
      */
     public static double square(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4) {
-        return 0;
+        double s1 = Math.abs((x1 - x3) * (y2 - y3) - (x2 - x3) * (y1 - y3)) / 2d;
+        double s2 = Math.abs((x1 - x4) * (y3 - y4) - (x3 - x4) * (y1 - y4)) / 2d;
+
+        return s1 + s2;
     }
 
 }
