9c38bc744c52ad2c3305aa678236211a17a67ccf
Sl <S@expo.com>
2020-05-05 14:42:56 +0300

    complete IntegerTask

--- src/main/java/ru/spsuace/helloworld/task1/IntegerTask.java
+++ src/main/java/ru/spsuace/helloworld/task1/IntegerTask.java
@@ -14,7 +14,11 @@ public class IntegerTask {
      * Пример: (5) -> 15
      */
     public static int sum(int n) {
-        return 0;
+        int result = 0;
+        for (int i = 1; i <= n; i++) {
+            result = result + i;
+        }
+        return result;
     }
 
     /**
@@ -24,7 +28,23 @@ public class IntegerTask {
      * Пример: (10, 3, 2) -> 8
      */
     public static int snake(int height, int top, int bottom) {
-        return 0;
+        int days = 0;
+        int finished = 0;
+        if (top >= height) {
+            return 1;
+        }
+        if (bottom >= top) {
+            return Integer.MAX_VALUE;
+        }
+        while (finished < height) {
+            days++;
+            finished=finished + top;
+            if (finished >= height) {
+                return days;
+            }
+            finished -= bottom;
+        }
+        return days;
     }
 
     /**
@@ -32,7 +52,14 @@ public class IntegerTask {
      * Пример: (454355, 3) -> 3
      */
     public static int kDecimal(int n, int order) {
-        return 0;
+        int result = 0;
+        int lastDivided = Math.abs(n);
+        for (int i = 1; i <= order; i++) {
+            result = lastDivided % 10;
+            lastDivided /= 10;
+        }
+
+        return result;
     }
 
 
@@ -41,6 +68,10 @@ public class IntegerTask {
      * Пример: (5) -> 120
      */
     public static long factorial(byte n) {
-        return 0;
+        long result = 1;
+        for (byte i = 1; i <= n; i++) {
+            result = result * i;
+        }
+        return result;
     }
 }
